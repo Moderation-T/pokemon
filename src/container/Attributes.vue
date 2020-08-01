7154b8fdad52b91c90130678cf9b33d18940b6e3
yizhiyu <yizhiyu_web@163.com>
2020-08-01 17:17:41 +0800

    属性相克 graph 图展示

--- src/container/Attributes.vue
+++ src/container/Attributes.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- 调用v-chart组件:渲染折线图 -->
-    <v-chart :options="myline"></v-chart>
+    <v-chart :options="myGraph"></v-chart>
   </div>
 </template>
 
@@ -19,13 +19,13 @@
 
 <script>
 import ECharts from "vue-echarts";
-// echarts-gl包含大部分图表所需要的依赖模块
-// import "echarts-gl";
-
 // 按需引入不同的图表需要的依赖
-import 'echarts/lib/chart/line';
-import 'echarts/lib/component/polar';
-import 'echarts/lib/chart/gauge';
+import "echarts/lib/chart/line";
+import "echarts/lib/component/polar";
+import "echarts/lib/chart/graph";
+
+// 所需宝可梦属性相克的数据
+import attributesJson from "../data/attributes.json";
 
 export default {
   name: "HelloWorld",
@@ -33,67 +33,97 @@ export default {
     "v-chart": ECharts
   },
   data() {
+    const nodesColors = [
+      "#9199A1",
+      "#CD406A",
+      "#8EA8DE",
+      "#A96AC8",
+      "#D87844",
+      "#C5B78B",
+      "#91C02E",
+      "#9F9FEC",
+      "#5A8DA1",
+      "#FE9C54",
+      "#77BBFF",
+      "#62BB5A",
+      "#F4D23C",
+      "#F97178",
+      "#73CEBF",
+      "#0A6DC2",
+      "#5A5365"
+    ];
+    const lineColors = { x0: "black", "x1/2": "red", x2: "green" };
+    const nodes = Object.keys(attributesJson).map((item, index) => ({
+      name: item,
+      itemStyle: { color: nodesColors[index] },
+      category: index
+    }));
+    const links = [];
+    for (const key in attributesJson) {
+      if ({}.hasOwnProperty.call(attributesJson, key)) {
+        const Attribute = attributesJson[key];
+        for (const _key in Attribute) {
+          if ({}.hasOwnProperty.call(Attribute, _key)) {
+            const element = Attribute[_key];
+            if (element.length !== 0) {
+              element.map(target => {
+                links.push({
+                  source: key,
+                  target,
+                  name: _key,
+                  lineStyle: { color: lineColors[_key] }
+                });
+              });
+            }
+          }
+        }
+      }
+    }
+
     return {
       //堆叠折线图所需参数对象
-      myline: {
+      myGraph: {
         title: {
-          text: "折线图堆叠"
+          text: "知识图谱"
         },
-        tooltip: {
-          trigger: "axis"
-        },
-        legend: {
-          data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"]
-        },
-        grid: {
-          left: "3%",
-          right: "4%",
-          bottom: "3%",
-          containLabel: true
-        },
-        toolbox: {
-          feature: {
-            saveAsImage: {}
-          }
-        },
-        xAxis: {
-          type: "category",
-          boundaryGap: false,
-          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
-        },
-        yAxis: {
-          type: "value"
-        },
+        // legend: [
+        //   {
+        //     // selectedMode: 'single',
+        //     data: categories.map(x => x.name)
+        //     // icon: 'circle'
+        //   }
+        // ],
         series: [
           {
-            name: "邮件营销",
-            type: "line",
-            stack: "总量",
-            data: [120, 132, 101, 134, 90, 230, 210]
-          },
-          {
-            name: "联盟广告",
-            type: "line",
-            stack: "总量",
-            data: [220, 182, 191, 234, 290, 330, 310]
-          },
-          {
-            name: "视频广告",
-            type: "line",
-            stack: "总量",
-            data: [150, 232, 201, 154, 190, 330, 410]
-          },
-          {
-            name: "直接访问",
-            type: "line",
-            stack: "总量",
-            data: [320, 332, 301, 334, 390, 330, 320]
-          },
-          {
-            name: "搜索引擎",
-            type: "line",
-            stack: "总量",
-            data: [820, 932, 901, 934, 1290, 1330, 1320]
+            type: "graph",
+            layout: "force",
+            symbolSize: 58,
+            draggable: true,
+            roam: true,
+            focusNodeAdjacency: true,
+            // categories: categories,
+            edgeSymbol: ["", "arrow"],
+            // edgeSymbolSize: [80, 10],
+            edgeLabel: {
+              normal: {
+                show: true,
+                textStyle: {
+                  fontSize: 20
+                },
+                formatter(x) {
+                  return x.data.name;
+                }
+              }
+            },
+            label: {
+              show: true
+            },
+            force: {
+              repulsion: 2000,
+              edgeLength: 120
+            },
+            data: nodes,
+            links: links
           }
         ]
       }
