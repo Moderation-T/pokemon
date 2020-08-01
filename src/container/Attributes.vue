654ab7a4e8011ec1e230cf7e4a7e3042fe28e805
yizhiyu <yizhiyu_web@163.com>
2020-08-01 21:45:00 +0800

    拆分效果不好与效果超群的显示

--- src/container/Attributes.vue
+++ src/container/Attributes.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <!-- 调用v-chart组件:渲染折线图 -->
-    <v-chart :options="myGraph"></v-chart>
+    <button @click="changeType(2)">效果超群</button>
+    <button @click="changeType(0)">效果不好</button>
+    <v-chart :options="type===0?myGraph0:myGraph2"></v-chart>
   </div>
 </template>
 
@@ -12,8 +14,8 @@
  * don't forget to provide a size for the container).
  */
 .echarts {
-  width: 1000px;
-  height: 1000px;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
 
@@ -30,7 +32,7 @@ import attributesJson from "../data/attributes.json";
 export default {
   name: "HelloWorld",
   components: {
-    "v-chart": ECharts
+    "v-chart": ECharts,
   },
   data() {
     const nodesColors = [
@@ -50,30 +52,46 @@ export default {
       "#F97178",
       "#73CEBF",
       "#0A6DC2",
-      "#5A5365"
+      "#5A5365",
     ];
     const lineColors = { x0: "black", "x1/2": "red", x2: "green" };
     const nodes = Object.keys(attributesJson).map((item, index) => ({
       name: item,
       itemStyle: { color: nodesColors[index] },
-      category: index
+      category: index,
     }));
-    const links = [];
+    // 效果不好 / 无效
+    const links0 = [];
+    // 效果超群
+    const links2 = [];
     for (const key in attributesJson) {
       if ({}.hasOwnProperty.call(attributesJson, key)) {
         const Attribute = attributesJson[key];
         for (const _key in Attribute) {
           if ({}.hasOwnProperty.call(Attribute, _key)) {
             const element = Attribute[_key];
-            if (element.length !== 0) {
-              element.map(target => {
-                links.push({
-                  source: key,
-                  target,
-                  name: _key,
-                  lineStyle: { color: lineColors[_key] }
+            if (_key === "x2") {
+              if (element.length !== 0) {
+                element.map((target) => {
+                  links2.push({
+                    source: key,
+                    target,
+                    name: _key,
+                    lineStyle: { color: lineColors[_key] },
+                  });
                 });
-              });
+              }
+            } else {
+              if (element.length !== 0) {
+                element.map((target) => {
+                  links0.push({
+                    source: key,
+                    target,
+                    name: _key,
+                    lineStyle: { color: lineColors[_key] },
+                  });
+                });
+              }
             }
           }
         }
@@ -81,10 +99,11 @@ export default {
     }
 
     return {
-      //堆叠折线图所需参数对象
-      myGraph: {
+      type: 0,
+      // 效果不好
+      myGraph0: {
         title: {
-          text: "知识图谱"
+          text: "知识图谱",
         },
         // legend: [
         //   {
@@ -104,30 +123,98 @@ export default {
             // categories: categories,
             edgeSymbol: ["", "arrow"],
             // edgeSymbolSize: [80, 10],
-            edgeLabel: {
+            // edgeLabel: {
+            //   normal: {
+            //     show: true,
+            //     textStyle: {
+            //       fontSize: 30,
+            //     },
+            //     formatter(x) {
+            //       return x.data.name;
+            //     },
+            //   },
+            // },
+            lineStyle: {
               normal: {
-                show: true,
-                textStyle: {
-                  fontSize: 20
-                },
-                formatter(x) {
-                  return x.data.name;
-                }
-              }
+                width: 2,
+                shadowColor: "none",
+                curveness: 0.1,
+              },
             },
             label: {
-              show: true
+              show: true,
             },
             force: {
               repulsion: 2000,
-              edgeLength: 120
+              edgeLength: 120,
             },
+
             data: nodes,
-            links: links
-          }
-        ]
-      }
+            links: links0,
+          },
+        ],
+      },
+      // 效果超群
+      myGraph2: {
+        title: {
+          text: "知识图谱",
+        },
+        // legend: [
+        //   {
+        //     // selectedMode: 'single',
+        //     data: categories.map(x => x.name)
+        //     // icon: 'circle'
+        //   }
+        // ],
+        series: [
+          {
+            type: "graph",
+            layout: "force",
+            symbolSize: 58,
+            draggable: true,
+            roam: true,
+            focusNodeAdjacency: true,
+            // categories: categories,
+            edgeSymbol: ["", "arrow"],
+            // edgeSymbolSize: [80, 10],
+            // edgeLabel: {
+            //   normal: {
+            //     show: true,
+            //     textStyle: {
+            //       fontSize: 30,
+            //     },
+            //     formatter(x) {
+            //       return x.data.name;
+            //     },
+            //   },
+            // },
+            lineStyle: {
+              normal: {
+                width: 2,
+                shadowColor: "none",
+                curveness: 0.1,
+              },
+            },
+            label: {
+              show: true,
+            },
+            force: {
+              repulsion: 2000,
+              edgeLength: 120,
+            },
+
+            data: nodes,
+            links: links2,
+          },
+        ],
+      },
     };
-  }
+  },
+
+  methods: {
+    changeType(type) {
+      this.type = type;
+    },
+  },
 };
 </script>
